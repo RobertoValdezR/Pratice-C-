6feea187a7ca44f9de00a9ef68304688ead38021
RobertoValdez <robertoalejandrovaldezrodrigue>
2025-08-18 09:47:45 -0400

    Primer ejercicios

--- Pratice-C#/Program.cs
+++ Pratice-C#/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/* Topic 1: Store and Retrieve Data Using Literal and Variable Values in C#
+
+Product information
+Create variables for a product: name, price, and quantity. Print all the information in one line.
+
+Rectangle area
+Store the width and height of a rectangle in variables. Calculate the area and print it.
+
+Personal info
+Create variables with your name, age, and country. Print them in a single sentence. */
+
+string name = "Marcos";
+double precio = 25.9;
+int cantidad = 12;
+
+Console.Write(name + precio + cantidad);
