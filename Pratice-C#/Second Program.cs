f7248ac3caf9fd23ab07d8375077a8ccea833fcd
RobertoValdez <robertoalejandrovaldezrodrigue>
2025-08-18 10:55:14 -0400

    Now, I declared variable and i used interpolation.

--- Pratice-C#/Second Program.cs
+++ Pratice-C#/Second Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿/* Topic 2: Perform Basic String Formatting in C#
+
+String interpolation
+Ask the user for their name and their favorite sport. Display:
+Hello [Name], your favorite sport is [Sport].
+
+Numeric formatting
+Declare a variable with a decimal number (e.g., 1234.5678). Show the number as currency and with 2 decimals.
+
+Aligned text
+Display a small table in console using string formatting:Topic 2: Perform Basic String Formatting in C#
+
+String interpolation
+Ask the user for their name and their favorite sport. Display:
+Hello [Name], your favorite sport is [Sport].
+
+Numeric formatting
+Declare a variable with a decimal number (e.g., 1234.5678). Show the number as currency and with 2 decimals.
+
+Aligned text
+Display a small table in console using string formatting */
+
+//  First Exercise:  String interpolation
+
+/* string name;
+name = "Roberto";
+string sport;
+sport = "Soccer";
+
+Console.WriteLine($"Hi {name}, your favorite sport is {sport}"); */
+
